d9cc40bc05e627e6092686e07362cf2d56067c7e
Oleh Tolstel <xtolstel@stuba.sk>
2025-08-17 17:42:30 +0200

    Mihon test 2

---
+++ src/es/senshimanga/src/eu/kanade/tachiyomi/extension/es/senshimanga/SenshiManga.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.es.senshimanga
+
+import eu.kanade.tachiyomi.multisrc.lectormoe.LectorMoe
+
+class SenshiManga : LectorMoe(
+    "Senshi Manga",
+    "https://senshimanga.capibaratraductor.com",
+    "es",
+    apiBaseUrl = "https://api.capibaratraductor.com",
+)
